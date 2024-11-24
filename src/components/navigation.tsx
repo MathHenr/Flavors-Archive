2726b2efa097d5d0ab7fb7d2bd3e48c2232ac2cf
Matheus Henrique <matheusobrinho2k20@gmail.com>
2024-11-24 01:18:26 -0300

    Coloquei o logo e adicionei uns efeitos de hover no navegadores.

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,12 +1,38 @@
+import { Search } from "lucide-react";
+
+const routes = [
+    "Home",
+    "Recipe",
+    "Comments",
+    "About",
+]
+
 export function Navigation() {
+    
     return (
-        <nav>
-            <ul>
-                <li>Home</li>
-                <li>Recipe</li>
-                <li>Comments</li>
-                <li>About</li>
+        <nav
+            className="max-w-screen-2xl mx-auto flex items-center justify-between px-8 py-4 font-poppins"
+        >
+            <img 
+                className="w-40"
+                src="/logo.svg"
+                alt="Logo"
+            />
+            
+            <ul
+                className="flex items-center gap-10 -ml-20"
+            >
+                {routes.map((route) => (
+                    <li
+                        key={route}
+                        className="px-2 rounded-sm transition-all ease-in duration-100 hover:scale-105 hover:cursor-pointer hover:shadow-[0px_10px_5px_-6px_rgba(0,0,0,0.15)]"
+                    >
+                        {route}
+                    </li>
+                ))}
             </ul>
+
+            <Search />
         </nav>
     )
 }
