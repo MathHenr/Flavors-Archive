cecfd6d58cd6e9d4b2ede29e36559f91139baf98
Matheus Henrique <matheusobrinho2k20@gmail.com>
2024-11-24 01:28:36 -0300

    Adicionei um menu button para mobile.

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,4 +1,4 @@
-import { Search } from "lucide-react";
+import { Search, Menu } from "lucide-react";
 
 const routes = [
     "Home",
@@ -13,16 +13,26 @@ export function Navigation() {
         <nav
             className="max-w-screen-2xl mx-auto flex items-center justify-between px-8 py-4 font-poppins"
         >
-            <img 
-                className="w-40"
-                src="/logo.svg"
-                alt="Logo"
-            />
+            <span
+                className="flex items-center gap-4"
+            >
+                <Menu 
+                    className="flex lg:hidden size-8 rounded-sm transition-all ease-in duration-100 cursor-pointer hover:bg-slate-500 hover:text-slate-50"
+                />
+
+                <img 
+                    className="w-40"
+                    src="/logo.svg"
+                    alt="Logo"
+                />
+            </span>
+            
             
             <ul
-                className="flex items-center gap-10 -ml-20"
+                className="hidden lg:flex items-center gap-10 -ml-20"
             >
                 {routes.map((route) => (
+                    // lista para levar usuário a diferentes partes da página
                     <li
                         key={route}
                         className="px-2 rounded-sm transition-all ease-in duration-100 hover:scale-105 hover:cursor-pointer hover:shadow-[0px_10px_5px_-6px_rgba(0,0,0,0.15)]"
@@ -32,7 +42,7 @@ export function Navigation() {
                 ))}
             </ul>
 
-            <Search />
+            <Search/>
         </nav>
     )
 }
