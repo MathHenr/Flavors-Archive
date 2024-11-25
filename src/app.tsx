648917b3a8a843931a3b6c35f5586e4068d0bffe
Matheus Henrique <matheusobrinho2k20@gmail.com>
2024-11-24 21:30:04 -0300

    Rascunho de um bloco de receita finalizado.

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,9 @@
 import { Button } from "./components/button";
 import { Navigation } from "./components/navigation";
 
+import { Clock4, UserRound, ChartNoAxesColumn } from "lucide-react";
+
+
 export function App() {
 
   return (
@@ -50,21 +53,66 @@ export function App() {
         className="mt-24 py-2"
       >
         <div
-          className="max-w-screen-2xl mx-auto px-4 flex items-center justify-between"
+          className="max-w-screen-2xl mx-auto px-4 flex flex-col"
         >
-          <span>
-            <h1
-              className="text-3xl font-semibold"
+          <div className="flex items-center justify-between">
+            <span>
+              <h1
+                className="text-3xl font-semibold"
+              >
+                Discover, Create and Share
+              </h1>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </p>
+            </span>
+            <Button>
+              Create
+            </Button>
+          </div>
+
+          {/* galeria de receitas */}
+          <div
+            className="grid grid-cols-1 lg:grid-cols-3 py-2 gap-3 place-items-center px-4 pb-[300px]"
+          >
+            {/* receita */}
+            <div
+              className="w-[368px] max-w-[400px] rounded-t-3xl overflow-hidden flex flex-col lg:items-start mt-5 transition-all linear duration-300 hover:shadow-md"
             >
-              Discover, Create and Share
-            </h1>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            </p>
-          </span>
-          <Button>
-            Create
-          </Button>
+              <img src="/png/food-gallery.jpg" alt="Food" className="w-full"/>
+              <div
+                className="bg-slate-200/95 -mt-4 w-full text-base font-medium font-poppins flex flex-col p-2"
+              >
+                <span
+                  className="flex items-center justify-around mb-4"
+                >
+                  <p className="text-xs text-green-700/95 flex flex-col items-center justify-center">
+                    <Clock4 className="text-green-700 size-4"/>
+                    4 min
+                  </p>
+                  <p className="text-xs text-green-700/95 flex flex-col items-center justify-center">
+                    <UserRound className="text-green-700 size-4"/>
+                    3 servings
+                  </p>
+                  <p className="text-xs text-green-700/95 flex flex-col items-center justify-center">
+                    <ChartNoAxesColumn className="text-green-700 size-4"/>
+                    ease
+                  </p>
+                </span>
+                
+                <span>
+                  Recipe name
+                </span>
+                <a 
+                  className="max-w-20 text-xs text-mustard/80 hover:text-mustard hover:underline"
+                  href="*"
+                >
+                  View recipe
+                </a>
+              </div>
+            </div>
+            
+          </div>
         </div>
       </section>
     </>
